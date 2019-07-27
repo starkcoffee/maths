9822c5770601a847d27657110e42c21d64b25277
Duana Saskia <starkcoffee@users.noreply.github.com>
2019-07-27 21:03:10 +1000

    Ripple carry working

--- arbitrary_precision.py
+++ arbitrary_precision.py
@@ -3,41 +3,50 @@ from functools import reduce
 
 
 def add(a, b):
+  c, d = (_prependZeroes(a, len(b)-len(a)), _prependZeroes(b, len(a)-len(b)))
+  return _rippleCarry([ x+y for x, y in zip(c, d) ])
 
-  c, d = (prependZeroes(a, len(b)-len(a)), prependZeroes(b, len(a)-len(b)))
+def _rippleCarry(a):
+  # carry is acc[0], result is acc[1] - assignment expressions in python 3.8
+  f = lambda acc, x:( (acc[0]+x) // 10, acc[1] + [(acc[0]+x) % 10] )
+  carry, result = reduce(f, reversed(a), (0, []))
+  result = list(reversed(result))
+  # since carry can be > 10, split it into its digits
+  carry = list(filter(lambda x: x!=0, map(lambda x: int(x), str(carry))))
+  return carry + result 
 
-  return [ x+y for x, y in zip(c, d) ]
-
-
-################################################################################
-# helper functions
-def prependZeroes(a, num_zeroes):
-  # itertools.repeat wd be faster but repeatability is more impt to me than speed
+def _prependZeroes(a, num_zeroes):
    return [0] * num_zeroes + a
 
-################################################################################
 class TestBig(unittest.TestCase):
 
-  def testPrependZeroesIgnoresNegative(self):
-    self.assertEqual(prependZeroes([1,2], -1), [1,2])
-
-  def testPrependZeroes(self):
-    self.assertEqual(prependZeroes([1], 5), [0,0,0,0,0,1])
-    self.assertEqual(prependZeroes([1,2], 1), [0,1,2])
-    self.assertEqual(prependZeroes([1,2], 0), [1,2])
-    self.assertEqual(prependZeroes([], 1), [0])
-
   def testAdd(self):
+    #self.assertEqual(add([], []), [0])
+    #self.assertEqual(add([1], []), [1])
+    #self.assertEqual(add([], [1]), [1])
     self.assertEqual(add([1],[1]), [2])
     self.assertEqual(add([1,2,3],[1]), [1,2,4])
     self.assertEqual(add([1],[1,2,3]), [1,2,4])
     self.assertEqual(add([1,2,3],[1,2,3]), [2,4,6])
-    self.assertEqual(add([3,0,0],[1,2,3]), [2,2,3])
+    self.assertEqual(add([9,9,9],[9,9,9]), [1,9,9,8])
 
-  @unittest.skip
-  def testAddHandlesEmpties(self):
-    self.assertEqual(add([], []), [0])
-    self.assertEqual(add([1], []), [1])
-    self.assertEqual(add([], [1]), [1])
+  def testRippleCarry(self):
+    self.assertEqual(_rippleCarry([1, 11]), [2, 1]) 
+    self.assertEqual(_rippleCarry([0]), [0]) 
+    self.assertEqual(_rippleCarry([1,2]), [1,2]) 
+    self.assertEqual(_rippleCarry([42,2]), [4,2,2]) 
+    self.assertEqual(_rippleCarry([2,342]), [3,6,2]) 
+    self.assertEqual(_rippleCarry([1, 2,53342]), [5,3,4,6,2]) 
+    self.assertEqual(_rippleCarry([642,2]), [6,4,2,2]) 
+
+  def testPrependZeroes(self):
+    self.assertEqual(_prependZeroes([1], 5), [0,0,0,0,0,1])
+    self.assertEqual(_prependZeroes([1,2], 1), [0,1,2])
+    self.assertEqual(_prependZeroes([1,2], 0), [1,2])
+    self.assertEqual(_prependZeroes([], 1), [0])
+
+  def testPrependZeroesIgnoresNegative(self):
+    self.assertEqual(_prependZeroes([1,2], -1), [1,2])
+
 if __name__ == '__main__':
     unittest.main()
