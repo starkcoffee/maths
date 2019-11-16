fe91becbeb6018f8dbba6dbdb947d3278df2bde7
Duana Saskia <starkcoffee@users.noreply.github.com>
2019-11-17 04:15:15 +1000

    Finish specifying add tests in test_anum

--- very_arbitrary_precision/test_anum.py
+++ very_arbitrary_precision/test_anum.py
@@ -19,22 +19,22 @@ class AnumTest(unittest.TestCase):
     self.assertEqual(n.f, [2,3,4])
 
 
-  @unittest.skip("")
   def testCanAddWholeNumbersTogether(self):
-    self.assertEqual(add([], []), (0, []))
-    self.assertEqual(Anum([1]) + Anum([1]), Anum([2]))
-    self.assertEqual(Anum([9,9,9]) + Anum([1]), Anum([1,0,0,0]))
-    self.assertEqual(Anum([6,6,6]) + Anum([6,6,6]), Anum([1,3,3,2]))
-    self.assertEqual(add([1], []), (0, [1]))
-    self.assertEqual(add([], [1]), (0, [1]))
-    self.assertEqual(add([1],[1]), (0, [2]))
-    self.assertEqual(add([1,2,3],[1]), (0, [1,2,4]))
-    self.assertEqual(add([1],[1,2,3]), (0, [1,2,4]))
-    self.assertEqual(add([1,2,3],[1,2,3]), (0, [2,4,6]))
-    self.assertEqual(add([9,9,9],[9,9,9]), (1, [9,9,8]))
-    self.assertEqual(add([243,53,9],[42,0,18]), (29, [0,5,7]))
-    self.assertEqual(add([100],[1]), (10, [1]))
+    self.assertEqual(Anum([]) + Anum([]), Anum([0]))
+    self.assertEqual(Anum([1]) + Anum([]), Anum([1]))
+    self.assertEqual(Anum([]) + Anum([1]), Anum([1]))
 
+    self.assertEqual(Anum([0]) + Anum([0]), Anum([0]))
+    self.assertEqual(Anum([1]) + Anum([1]), Anum([2]))
+
+    self.assertEqual(Anum([9,9,9]) + Anum([1]), Anum([1,0,0,0]))
+    self.assertEqual(Anum([1]) + Anum([9,9,9]), Anum([1,0,0,0]))
+    self.assertEqual(Anum([6,6,6]) + Anum([6,6,6,7]), Anum([7,3,3,3]))
+
+    self.assertEqual(Anum([100]) + Anum([1]), Anum([1,0,1]))
+    self.assertEqual(Anum([243,53,9]) + Anum([42,0,18]), Anum([2,9,0,5,7]))
+
+    self.assertEqual(Anum([1]*10000) + Anum([1]), Anum([1]*9999 + [2]))
 
   @unittest.skip("")
   def testCanAddRationalNumbersTogether(self):
