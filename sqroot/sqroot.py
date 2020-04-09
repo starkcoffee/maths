ed34d0d78a42de35e52ec87cf26442975ee814d2
Duana Saskia <starkcoffee@users.noreply.github.com>
2020-04-10 09:20:52 +1000

    sqroot: refactor powOf10

--- sqroot/sqroot.py
+++ sqroot/sqroot.py
@@ -3,16 +3,27 @@ def digByDigSqroot(n, desiredDigits):
   powersOf100 = list(_generatePowersOf100(n))
   highestPowerOfHundered = powersOf100.pop()
 
-  powOf10 = pow(10, len(powersOf100))
-  x = _closestIntSqrt(highestPowerOfHundered) * powOf10
+  powOf10 = len(powersOf100)
+  x = _closestIntSqrt(highestPowerOfHundered) * pow(10, powOf10)
   remainder = n - x*x
 
   for i in range(desiredDigits-1):
-    powOf10 = powOf10/10
+    powOf10 = powOf10 -1
     (y, remainder) = _findY(x, powOf10, remainder)
     x = x+y
   return x
 
+def _findY(x, powerOf10, remainder):
+  print("args", x, powerOf10, remainder)
+  newRemainder = remainder
+  for i in range(10):
+    y = i* pow(10, powerOf10)
+    diff = 2*x*y + y*y 
+    if diff > remainder:
+      return ((i-1)* pow(10, powerOf10), newRemainder)
+    else:
+      newRemainder = remainder - diff
+  return (i*pow(10, powerOf10), newRemainder)
 
 def _hundreds(n):
   while True:
@@ -35,17 +46,5 @@ def _generatePowersOf100(n):
   while n > 0:
     yield n % 100
     n = n // 100
-
-def _findY(x, powerOf10, remainder):
-  print("args", x, powerOf10, remainder)
-  newRemainder = remainder
-  for i in range(10):
-    y = i*powerOf10
-    diff = 2*x*y + y*y 
-    if diff > remainder:
-      return ((i-1)*powerOf10, newRemainder)
-    else:
-      newRemainder = remainder - diff
-  return (i*powerOf10, newRemainder)
 
 
