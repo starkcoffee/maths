1e36de6fab1e2f460ceddd1ba76d243e87961a57
Duana Saskia <starkcoffee@users.noreply.github.com>
2020-10-04 21:06:27 +1000

    necklaces: add necklace_count method

--- maths/necklaces.py
+++ maths/necklaces.py
@@ -1,19 +1,23 @@
 from itertools import product
 from functools import reduce
 
+def necklace_count(num_colours, length):
+  return len(necklaces(range(num_colours), length))
+
 def necklaces(iterable, num):
-  
   necklaces = set()
   for p in product(iterable, repeat=num):
     if necklaces.isdisjoint(rotations(p)): 
       necklaces.add(p)
 
+  return necklaces
   str_necklaces = {''.join(n) for n in necklaces }
   return str_necklaces
 
 def rotations(permutation):
   rotate = lambda perm, offset: tuple( perm[(i+offset) % len(perm)] for i in range(len(perm)) )
   return { rotate(permutation, i) for i in range(len(permutation)) }
+
 
 # returns tree of permutations of symbols as a list of tuples
 def generate_tree_breadth_first(symbols, depth):
@@ -30,6 +34,7 @@ def generate_tree_breadth_first_acc(tupelised_symbols, depth, tree_acc):
   next_generation_of_tree = [ a+b for a in tree_acc for b in tupelised_symbols ]
   return generate_tree_breadth_first_acc(tupelised_symbols, depth - 1, next_generation_of_tree) 
 
+# returns tree of permutations of symbols as a list of tuples
 def generate_tree_depth_first(symbols, depth):
   if depth == 0:
     return []
--- test/test_necklaces.py
+++ test/test_necklaces.py
@@ -13,10 +13,8 @@ def test_generate_tree_depth_first():
   assert generate_tree_depth_first(['A','B'], 2) == [('A','A'),('A','B'),('B','A'),('B','B')]
   assert generate_tree_depth_first(['A','B','C'], 3)[9] == ('B','A','A')
 
-def test_result():
-  assert len(necklaces('ABCD',4)) == 70
-
 def test_necklaces():
+  assert necklaces('ABC', 0) == {}
   assert necklaces('ABC', 1) == {'A', 'B', 'C'}
   assert necklaces('ABC', 2) == {'AA', 'AB', 'AC','BB','BC','CC'}
   assert necklaces('ABC', 3) == {'AAA','AAB','AAC','ABB','ABC','ACB','ACC','BBB','BBC','BCC','CCC'}
