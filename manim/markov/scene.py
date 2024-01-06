bea220ac06366f8e1c13a93e98ed4f6ad08a255c
Duana Saskia <starkcoffee@users.noreply.github.com>
2024-01-06 19:54:58 +1000

    Slow down animation

--- manim/markov/scene.py
+++ manim/markov/scene.py
@@ -11,18 +11,18 @@ class CreateFlow(Scene):
       rect2_start.set_color(BLUE)
       rect2_start.set_fill(BLUE, opacity=0.8)
 
-      rect1_start.shift(LEFT).shift(LEFT).shift(LEFT)
-      rect2_start.shift(RIGHT).shift(RIGHT).shift(RIGHT)
+      rect1_start.move_to(LEFT * 3)
+      rect2_start.move_to(RIGHT * 3)
       rect2_start.align_to(rect1_start, direction=DOWN)
 
       rect1_end = Rectangle(width=1.0, height=2.0)
       rect1_end.set_color(ORANGE)
       rect1_end.set_fill(ORANGE, opacity=0.8)
-      rect1_end.shift(LEFT).shift(LEFT).shift(LEFT)
+      rect1_end.move_to(LEFT * 3)
       rect1_end.align_to(rect1_start, direction=DOWN)
 
       self.add(rect1_start)
       self.add(rect2_start)
       self.wait(2)
-      self.play(Transform(rect1_start, rect1_end))
+      self.play(Transform(rect1_start, rect1_end), run_time=3)
       self.wait(3)
