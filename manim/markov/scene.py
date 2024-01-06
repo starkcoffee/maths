4806409dbdb3a1aeb19b018c0d1a5f0328850360
Duana Saskia <starkcoffee@users.noreply.github.com>
2024-01-06 19:47:03 +1000

    Rectangle size animates

--- manim/markov/scene.py
+++ manim/markov/scene.py
@@ -3,17 +3,26 @@ from manim import *
 
 class CreateFlow(Scene):
     def construct(self):
-      rect1 = Rectangle(width=1.0, height=4.0)
-      rect1.set_color(ORANGE)
-      rect1.set_fill(ORANGE, opacity=0.8)
-      rect2 = Rectangle(width=1.0, height=2.0)
-      rect2.set_color(BLUE)
-      rect2.set_fill(BLUE, opacity=0.8)
+      rect1_start = Rectangle(width=1.0, height=5.0)
+      rect1_start.set_color(ORANGE)
+      rect1_start.set_fill(ORANGE, opacity=0.8)
 
-      rect1.shift(LEFT).shift(LEFT)
-      rect2.shift(RIGHT).shift(RIGHT)
-      rect2.align_to(rect1, direction=DOWN)
+      rect2_start = Rectangle(width=1.0, height=2.0)
+      rect2_start.set_color(BLUE)
+      rect2_start.set_fill(BLUE, opacity=0.8)
 
-      self.add(rect1)
-      self.add(rect2)
-      self.wait(5)
+      rect1_start.shift(LEFT).shift(LEFT).shift(LEFT)
+      rect2_start.shift(RIGHT).shift(RIGHT).shift(RIGHT)
+      rect2_start.align_to(rect1_start, direction=DOWN)
+
+      rect1_end = Rectangle(width=1.0, height=2.0)
+      rect1_end.set_color(ORANGE)
+      rect1_end.set_fill(ORANGE, opacity=0.8)
+      rect1_end.shift(LEFT).shift(LEFT).shift(LEFT)
+      rect1_end.align_to(rect1_start, direction=DOWN)
+
+      self.add(rect1_start)
+      self.add(rect2_start)
+      self.wait(2)
+      self.play(Transform(rect1_start, rect1_end))
+      self.wait(3)
